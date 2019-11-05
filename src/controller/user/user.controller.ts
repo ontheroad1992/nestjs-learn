8e4e053531a165bd0a11bee99379e4f162972cd3
ontheroad <pengshba@163.com>
2019-11-05 10:51:52 +0800

    Update 完成控制的学习

--- src/controller/user/user.controller.ts
+++ src/controller/user/user.controller.ts
@@ -1,36 +1,10 @@
-import { Controller, Get, Req, Post, HttpCode, Res, Redirect, Query } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './create-user.dto'
 
 @Controller('user')
 export class UserController {
-  @Get()
-  findUser(): string {
-    return 'this is user api';
-  }
-
-  @Get('req')
-  findReq(@Req() request: Request): string {
-    // console.log(request);
-    // console.log('this is req');
-    return 'this is req';
-  }
-
   @Post()
-  @HttpCode(204)
-  create(@Res() response: Response): string {
-    // console.log()
-    response.send('this is http code 204');
-    return 'this is http code 200';
-  }
-
-  @Get('docs')
-  @Redirect('https://docs.nestjs.com', 302)
-  getDocs(@Query('ver') ver: string): string | object {
-    console.log(ver, 'sss');
-    if (ver && ver === '5') {
-      return { url: 'https://docs.nestjs.com/v5/' };
-    } else {
-      return '版本不存在';
-    }
+  createUser(@Body() createUserDto: CreateUserDto): object {
+    return createUserDto
   }
 }
